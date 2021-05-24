9691522ccd710ded94a4d3ad8e294ac61d751e7a
DhruvBhattathiri <73540837+DhruvBhattathiri@users.noreply.github.com>
2021-05-24 12:30:49 +0530

    Add files via upload

---
+++ webcam.js
@@ -0,0 +1,14 @@
+prediction = ""
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format: 'png',
+    png_quality:90
+});
+ 
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+
